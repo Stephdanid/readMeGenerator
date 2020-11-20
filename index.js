49f33644434b86544a37e72a6281c886356e745a
stephdanid <stephaniedanidominguez@gmail.com>
2020-11-19 22:32:57 -0600

    created writeToFile function, added test question to array, coded in Init function, able to generate read me file

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 // array of questions for user
-const questions = [
-
-];
+const inquirer = require('inquirer');
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js")
+const questions = [{
+    type: 'input',
+    message: 'What is your name?',
+    name: 'title',
+}];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (error) => {
+        if (error) throw error;
+        console.log("File written successfully");
+    })
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+        .then((response) => {
+            console.log(response)
+            writeToFile("README.md", generateMarkdown(response));
+        });
 }
 
 // function call to initialize program
 init();
+
+// to be continued https: //code.visualstudio.com/docs/languages/markdown
